829fe41f81dab45c12b91530a7de8f9abeec229c
DuypnaFX13348 <duypna0604@gmail.com>
2022-03-31 22:49:07 +0700

    Lab2.5

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
-const express = require('express');
+const express = require('express'); // import express.js được cài đặt từ npm install để hỗ trợ code nhanh hơn
+const bodyParser = require('body-parser'); // import body-parser được cái đặt từ npm install để hỗ trợ lấy giá trị của ô <input> trong thẻ <form>
+
 const app = express();
 
-// Middleware 1
-app.use('/',(req, res, next) => {
-    console.log('This always run!');
-    next();
-});
-// Middleware 2
+app.use(bodyParser.urlencoded({extended: false})); // sử dụng body-parser đã import vào từ dòng 2
+
+// Middleware với path là localhost3000/add-product
 app.use('/add-product',(req, res, next) => {
-    console.log('In the middleware!');
-    res.send('<h1>The Add Product Page!</h1>');
+    res.send('<form action="/product" method="POST" ><input type="text" name="tittle" /><button type="submit">Add Product</button></form>');
 });
-//Middleware 3
+// Middleware với path là localhost3000/product
+app.use('/product',(req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+//Middleware với path là localhost3000
 app.use('/',(req, res, next) => {
-    console.log('In the another middleware!');
     res.send('<h1>Hello from Express.js</h1>');
 });
 
