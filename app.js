22912c9872847f481142f683370a7030a921bb40
DuypnaFX13348 <duypna0604@gmail.com>
2022-03-31 20:48:19 +0700

    Lab2.2

--- app.js
+++ app.js
@@ -3,6 +3,15 @@ const http = require('http');
 const express = require('express');
 const app = express();
 
+app.use((req, res, next) => {
+    console.log('In the middleware!');
+    next(); // Cho phép request tiếp tục đến middleware tiếp theo. Nếu next() không được gọi thì dòng số 11 đến 13 sẽ không được thực hiện
+});
+
+app.use((req, res, next) => {
+    console.log('In the another middleware!');
+});
+
 const server = http.createServer(app);
 
 server.listen(3000);
