32016cc4c707b7f87f4597d7e8a32f8113ed1e23
DuypnaFX13348 <duypna0604@gmail.com>
2022-04-01 14:42:49 +0700

    Lab2.10

--- routes/admin.js
+++ routes/admin.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 // Middleware với path là localhost3000/admin/add-product vì ở app.js dòng 10 đã thêm path /admin ở đầu
 router.get('/add-product',(req, res, next) => {
-    res.send('<form action="/admin/product" method="POST" ><input type="text" name="tittle" /><button type="submit">Add Product</button></form>');
+    res.send('<form action="/admin/product" method="POST" ><input type="text" name="title" /><button type="submit">Add Product</button></form>');
 });
 // Middleware với path là localhost3000/admin/product vì ở app.js dòng 10 đã thêm path /admin ở đầu
 router.post('/product',(req, res, next) => {
