b22a28d923e864522c7c5d88d0cd2236fd1b4952
DuypnaFX13348 <duypna0604@gmail.com>
2022-04-01 15:09:18 +0700

    Lab2.11

--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,10 @@
 const express = require('express');
+const path = require('path');
 
 const router = express.Router();
-
-
 // Middleware với path là localhost3000/admin/add-product vì ở app.js dòng 10 đã thêm path /admin ở đầu
 router.get('/add-product',(req, res, next) => {
-    res.send('<form action="/admin/product" method="POST" ><input type="text" name="title" /><button type="submit">Add Product</button></form>');
+    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'))
 });
 // Middleware với path là localhost3000/admin/product vì ở app.js dòng 10 đã thêm path /admin ở đầu
 router.post('/product',(req, res, next) => {
--- routes/shop.js
+++ routes/shop.js
@@ -1,11 +1,10 @@
-const express = require('express');
+const express = require('express'); 
+const path = require('path'); // import path để hỗ trợ lấy file html dưới dòng 7
 
 const router = express.Router();
-
-
 //Middleware với path là localhost3000
 router.get('/',(req, res, next) => {
-    res.send('<h1>Hello from Express.js</h1>');
+    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html')); // sử dụng sendFile(path.join(__dirname, '../', 'views', 'shop.html')) để lấy file shop.html từ views
 });
 
 module.exports = router;
