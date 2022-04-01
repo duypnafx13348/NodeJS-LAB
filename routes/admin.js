fd3489fa20801ab67291ac8b2ec3bad1f42d860c
DuypnaFX13348 <duypna0604@gmail.com>
2022-04-01 23:00:34 +0700

    Lab3.1

--- routes/admin.js
+++ routes/admin.js
@@ -1,17 +1,21 @@
 const express = require('express');
 const path = require('path');
 
-const rootDir = require('../helpers/path'); // import path.js từ helpers để làm gắn gọn hơn code ở dòng số 9
+const rootDir = require('../helpers/path'); // import path.js từ helpers để sử lý render giao diện cho người dùng dưới dòng số 9
 
 const router = express.Router();
+const products = []; // tạo 1 cái biến với mảng rỗng để lấy giá trị của ô input bỏ vào
+
 // Middleware với path là localhost3000/admin/add-product vì ở app.js dòng 10 đã thêm path /admin ở đầu
 router.get('/add-product',(req, res, next) => {
     res.sendFile(path.join(rootDir, 'views', 'add-product.html')) // sử dụng sendFile(path.join(rootDir, 'views', 'add-product.html')) thay cho sendFile(path.join(__dirname, '../', 'views', 'add-product.html')) để lấy file add-product.html từ views
 });
+
 // Middleware với path là localhost3000/admin/product vì ở app.js dòng 10 đã thêm path /admin ở đầu
-router.post('/product',(req, res, next) => {
-    console.log(req.body);
+router.post('/add-product',(req, res, next) => {
+    products.push({title: req.body.title});
     res.redirect('/');
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,13 @@
 const express = require('express'); 
 const path = require('path'); // import path để hỗ trợ lấy file html dưới dòng 7
-const rootDir = require('../helpers/path');
+const rootDir = require('../helpers/path'); // import path.js từ helpers để sử lý render giao diện cho người dùng dưới dòng số 8
+const adminData = require('./admin');
 
 const router = express.Router();
+
 //Middleware với path là localhost3000
 router.get('/',(req, res, next) => {
+    console.log('shop.js', adminData.products);
     res.sendFile(path.join(rootDir, 'views', 'shop.html')); // sử dụng sendFile(path.join(rootDir, 'views', 'shop.html')) thay cho sendFile(path.join(__dirname, '../', 'views', 'shop.html')) để lấy file shop.html từ views
 });
 
