6c6ee10d8942b51889596cd96d2f067fcb480e64
DuypnaFX13348 <duypna0604@gmail.com>
2022-04-01 21:15:12 +0700

    Lab2.13

--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const path = require('path');
 
+const rootDir = require('../helpers/path'); // import path.js từ helpers để làm gắn gọn hơn code ở dòng số 9
+
 const router = express.Router();
 // Middleware với path là localhost3000/admin/add-product vì ở app.js dòng 10 đã thêm path /admin ở đầu
 router.get('/add-product',(req, res, next) => {
-    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html')) // sử dụng sendFile(path.join(__dirname, '../', 'views', 'add-product.html')) để lấy file add-product.html từ views
+    res.sendFile(path.join(rootDir, 'views', 'add-product.html')) // sử dụng sendFile(path.join(rootDir, 'views', 'add-product.html')) thay cho sendFile(path.join(__dirname, '../', 'views', 'add-product.html')) để lấy file add-product.html từ views
 });
 // Middleware với path là localhost3000/admin/product vì ở app.js dòng 10 đã thêm path /admin ở đầu
 router.post('/product',(req, res, next) => {
--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,11 @@
 const express = require('express'); 
 const path = require('path'); // import path để hỗ trợ lấy file html dưới dòng 7
+const rootDir = require('../helpers/path');
 
 const router = express.Router();
 //Middleware với path là localhost3000
 router.get('/',(req, res, next) => {
-    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html')); // sử dụng sendFile(path.join(__dirname, '../', 'views', 'shop.html')) để lấy file shop.html từ views
+    res.sendFile(path.join(rootDir, 'views', 'shop.html')); // sử dụng sendFile(path.join(rootDir, 'views', 'shop.html')) thay cho sendFile(path.join(__dirname, '../', 'views', 'shop.html')) để lấy file shop.html từ views
 });
 
 module.exports = router;
