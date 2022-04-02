e02d619d10296879f1caabda710465b3232bfa31
DuypnaFX13348 <duypna0604@gmail.com>
2022-04-02 14:32:40 +0700

    Lab3.2

--- routes/shop.js
+++ routes/shop.js
@@ -7,8 +7,7 @@ const router = express.Router();
 
 //Middleware với path là localhost3000
 router.get('/',(req, res, next) => {
-    console.log('shop.js', adminData.products);
-    res.sendFile(path.join(rootDir, 'views', 'shop.html')); // sử dụng sendFile(path.join(rootDir, 'views', 'shop.html')) thay cho sendFile(path.join(__dirname, '../', 'views', 'shop.html')) để lấy file shop.html từ views
+    res.render('shop');
 });
 
 module.exports = router;
