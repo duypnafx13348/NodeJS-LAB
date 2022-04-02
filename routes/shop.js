1fcfd0cdee2637628d3aa68f6dcb50659d29f3cb
DuypnaFX13348 <duypna0604@gmail.com>
2022-04-02 15:32:21 +0700

    Lab3.3

--- routes/shop.js
+++ routes/shop.js
@@ -1,13 +1,13 @@
 const express = require('express'); 
-const path = require('path'); // import path để hỗ trợ lấy file html dưới dòng 7
-const rootDir = require('../helpers/path'); // import path.js từ helpers để sử lý render giao diện cho người dùng dưới dòng số 8
 const adminData = require('./admin');
 
 const router = express.Router();
 
 //Middleware với path là localhost3000
 router.get('/',(req, res, next) => {
-    res.render('shop');
+    const products = adminData.products;
+    console.log(products);
+    res.render('shop', { prods: products, docTitle: 'shop' });
 });
 
 module.exports = router;
