1327838304eb153f034f01ce603513a6466c1821
DuypnaFX13348 <duypna0604@gmail.com>
2022-04-08 19:17:18 +0700

    Lab4.12

--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,8 @@
-const path = require('path');
-
 const express = require('express');
+const router = express.Router();
 
 const shopController = require('../controllers/shop');
 
-const router = express.Router();
 
 router.get('/', shopController.getIndex);
 
