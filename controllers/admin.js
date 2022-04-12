3b5aa077fb7c5bf01c254f1bafde97ec7a11352d
DuypnaFX13348 <duypna0604@gmail.com>
2022-04-12 22:22:03 +0700

    Lab4.18

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,7 +13,7 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(null, title, imageUrl, description, price);
+  const product = new Product(null, title, imageUrl, price, description);
   product.save();
   res.redirect('/');
 };
@@ -50,6 +50,8 @@ exports.postEditProduct = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
+  Product.deleteById(prodId);
+  res.redirect('/admin/products');
 };
 
 exports.getProducts = (req, res, next) => {
