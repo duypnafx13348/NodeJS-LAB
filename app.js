3b1c6a7e7094b1158f799e494696252cf0ee572e
DuypnaFX13348 <duypna0604@gmail.com>
2022-04-13 21:30:32 +0700

    Lab5.4

--- app.js
+++ app.js
@@ -5,15 +5,6 @@ const shopRoutes = require('./routes/shop');    //import từ file shop trong ro
 const path = require('path');
 const expressHbs = require('express-handlebars'); // import express-handlebars da cai dat vao
 const errorController = require('./controllers/error');     //import file error từ controllers vào để sử dụng dưới dòng 22
-const db = require('./util/database');
-
-db.execute('SELECT * FROM products')
-    .then(result => {
-        console.log('result', result);
-    })
-    .catch(err => {
-        console.log('error', err);
-    });
 
 const app = express();
 
