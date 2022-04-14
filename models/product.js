d23aabbd3ab558d2a040d0264fb2966e0e3e282c
DuypnaFX13348 <duypna0604@gmail.com>
2022-04-14 12:46:22 +0700

    Lab5.6

--- models/product.js
+++ models/product.js
@@ -11,7 +11,9 @@ module.exports = class Product {
   }
 
   save() {
-    
+    return db.execute('INSERT INTO products (title, imageUrl, price, description) VALUES (?, ?, ?, ?)',
+    [this.title, this.imageUrl, this.price, this.description]
+    );
   }
 
   static deleteById(id) {
