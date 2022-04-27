eba140da776ab33f561b6856484fc04309585924
DuypnaFX13348 <duypna0604@gmail.com>
2022-04-27 20:12:37 +0700

    Lab6.16

--- models/user.js
+++ models/user.js
@@ -4,14 +4,30 @@ const getDb = require("../util/database").getDb;
 const ObjectId = mongodb.ObjectId;
 
 class User {
-  constructor(name, email) {
+  constructor(name, email, cart, id) {
     this.name = name;
     this.email = email;
+    this.cart = cart; // {item: []}
+    this._id = id;
   }
 
   save() {
     const db = getDb();
     return db.collection("users").insertOne(this);
+  }
+
+  addToCart(product) {
+    const cartProduct = this.cart.item.findIndex((cb) => {
+      return cb._id === product._id;
+    });
+    const updatedCart = { item: [{ ...product, quantity: 1 }] };
+    const db = getDb();
+    return db
+      .collection("users")
+      .updateOne(
+        { id: new ObjectId(this._id) },
+        { $set: { cart: updatedCart } }
+      );
   }
 
   static findById(userId) {
