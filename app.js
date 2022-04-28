f8c80f152817f6314400c81d9d56afcd5b0baa9b
DuypnaFX13348 <duypna0604@gmail.com>
2022-04-28 15:08:24 +0700

    Lab7.3

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const shopRoutes = require("./routes/shop");
 const errorController = require("./controllers/error");
 
 const mongoose = require("mongoose");
-const User = require("./models/user");
+// const User = require("./models/user");
 
 const app = express();
 
@@ -17,14 +17,14 @@ app.set("views", "views");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use((req, res, next) => {
-  User.findById("6269747e14a8e5bf02393ead")
-    .then((user) => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
-      next();
-    })
-    .catch((err) => console.log(err));
-});
+// app.use((req, res, next) => {
+//   User.findById("6269747e14a8e5bf02393ead")
+//     .then((user) => {
+//       req.user = new User(user.name, user.email, user.cart, user._id);
+//       next();
+//     })
+//     .catch((err) => console.log(err));
+// });
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
