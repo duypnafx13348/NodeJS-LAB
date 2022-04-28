8dfb3f1b5ad77cf43cd5d2018c352a0e93dfb409
DuypnaFX13348 <duypna0604@gmail.com>
2022-04-28 17:04:31 +0700

    Lab7.8

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const shopRoutes = require("./routes/shop");
 const errorController = require("./controllers/error");
 
 const mongoose = require("mongoose");
-// const User = require("./models/user");
+const User = require("./models/user");
+const { getMaxListeners } = require("process");
 
 const app = express();
 
@@ -17,14 +18,14 @@ app.set("views", "views");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-// app.use((req, res, next) => {
-//   User.findById("6269747e14a8e5bf02393ead")
-//     .then((user) => {
-//       req.user = new User(user.name, user.email, user.cart, user._id);
-//       next();
-//     })
-//     .catch((err) => console.log(err));
-// });
+app.use((req, res, next) => {
+  User.findById("626a667513c1b912e6192fed")
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
@@ -36,6 +37,16 @@ mongoose
     "mongodb+srv://duypnafx13348:poeietiiup1@cluster0.ye7x7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
   )
   .then((result) => {
+    User.findOne().then((user) => {
+      if (!user) {
+        const user = new User({
+          name: "duypnafx13348",
+          email: "duypnafx13348@getMaxListeners.com",
+          cart: { items: [] },
+        });
+        user.save();
+      }
+    });
     app.listen(3000);
   })
   .catch((err) => console.log(err));
