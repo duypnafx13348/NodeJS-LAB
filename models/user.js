a89f48563d9b33bbcb8da8b69122cec0dc03d243
DuypnaFX13348 <duypna0604@gmail.com>
2022-04-29 21:36:05 +0700

    Lab7.13

--- models/user.js
+++ models/user.js
@@ -50,6 +50,17 @@ userSchema.methods.addToCart = function (product) {
   return this.save();
 };
 
+userSchema.methods.removeFromCart = function (productId) {
+  const updatedCartItems = this.cart.items.filter((item) => {
+    console.log("item.productId", item.productId);
+    console.log("item.productId.toString()", item.productId.toString());
+    return item.productId.toString() !== productId.toString();
+  });
+
+  this.cart.items = updatedCartItems;
+  return this.save();
+};
+
 module.exports = mongoose.model("User", userSchema);
 // const mongodb = require("mongodb");
 // const getDb = require("../util/database").getDb;
